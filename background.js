fca4b45d9d623a7f1d587f67d07920e0a99814eb
Sergei <soees84@gmail.com>
2026-07-01 12:36:03 +0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,2 @@
+// Can be empty for now, but useful for future features
+console.log('Random Wiki extension background loaded');
---
+++ config.js
@@ -0,0 +1,1 @@
+export const API_USER_AGENT = "WikiRandomInCategory/1.1 (https://github.com/sergey-smolin/wikipedia_random_in_category/)"
